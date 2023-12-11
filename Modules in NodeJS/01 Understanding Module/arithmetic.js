b591ab8346a4f07df4437f7142a8b629f47c1d4d
BhuvneshKumarAnand <bharatanand874@gmail.com>
2023-12-12 00:31:31 +0530

    Modules in NodeJS

---
+++ Modules in NodeJS/01 Understanding Module/arithmetic.js
@@ -0,0 +1,5 @@
+module.exports.sum = (x, y) => x + y;
+module.exports.divide = (x, y) => x / y;
+
+// 1. Common JS Module
+// 2. ES6 Module
---
+++ Modules in NodeJS/01 Understanding Module/result.js
@@ -0,0 +1,6 @@
+// call arithmetic function and print it on terminal
+
+// How to import a module
+
+const arithmeticModule = require("./arithmetic");
+console.log(arithmeticModule.sum(10, 5)); // Here sum is a property
