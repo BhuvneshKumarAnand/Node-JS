914cddb69ba9a20b5c4e24c8af0d6913ece8aa7f
BhuvneshKumarAnand <bharatanand874@gmail.com>
2023-12-12 01:15:41 +0530

    Modify

--- Modules in NodeJS/02 ES6 Modules/result.mjs
+++ Modules in NodeJS/02 ES6 Modules/result.mjs
@@ -1,6 +1,7 @@
 // Import the arithmetic.mjs file using ES6 Module
 
 import * as arithmeticModule from './arithmetic.mjs';
+// import { sum } from './arithmetic.mjs';
 
 console.log(arithmeticModule.num);
 console.log(arithmeticModule.sum(10, 20));
