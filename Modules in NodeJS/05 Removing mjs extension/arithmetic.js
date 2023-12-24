39d5882b8e8168ffdbd2888ba79396433ba908a6
BhuvneshKumarAnand <bharatanand874@gmail.com>
2023-12-24 13:37:11 +0530

    Type Module

---
+++ Modules in NodeJS/05 Removing mjs extension/arithmetic.js
@@ -0,0 +1,11 @@
+// Export these function using ES6 Modules
+
+export const num = 100;
+
+export function sum(x, y) {
+    return x + y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
---
+++ Modules in NodeJS/05 Removing mjs extension/result.js
@@ -0,0 +1,8 @@
+// Import the arithmetic.mjs file using ES6 Module
+
+import * as arithmeticModule from './arithmetic.js';
+// import { sum } from './arithmetic.mjs';
+
+console.log(arithmeticModule.num);
+console.log(arithmeticModule.sum(10, 20));
+// console.log(arithmeticModule.div(10, 20)); // This cannot not be imported
