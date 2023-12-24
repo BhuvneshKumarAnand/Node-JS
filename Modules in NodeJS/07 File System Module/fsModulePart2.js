8799f70cdaa1ddc9945123242263bd642a452a4d
BhuvneshKumarAnand <bharatanand874@gmail.com>
2023-12-24 15:56:25 +0530

    File System Module Part 2

--- Modules in NodeJS/07 File System Module/fsModulePart2.js
+++ Modules in NodeJS/07 File System Module/fsModulePart2.js
@@ -2,7 +2,10 @@ const fs = require('fs');
 
 // Create and wite a file
 
-fs.writeFileSync('employee.txt', 'Name: Bhuvnesh, Age:23, Education: B.Tech in ECE')
-
+try {
+    fs.writeFileSync('employee.txt', 'Name: Bhuvnesh, Age:23, Education: B.Tech in ECE')
+} catch (error) {
+    console.log(error);
+}
 
 console.log('This is another operation being performed');
