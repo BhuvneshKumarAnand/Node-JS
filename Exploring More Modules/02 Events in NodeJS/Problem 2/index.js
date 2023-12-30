2c11efa07ba8cfd98d9f83d18d0bddf476441fec
BhuvneshKumarAnand <bharatanand874@gmail.com>
2023-12-30 15:18:21 +0530

    More Problems on events

---
+++ Exploring More Modules/02 Events in NodeJS/Problem 2/index.js
@@ -0,0 +1,39 @@
+  import events from "events";
+
+  class FitnessTracker extends events.EventEmitter {
+      constructor() {
+          super();
+          this.progress = 0;
+          this.goal = 1000;
+      }
+
+      addExercise(exercise) {
+          // Write code to update the progress and emit a 'goalReached' event when the goal is reached
+          //update the progress based on calories burned
+          this.progress += exercise.caloriesBurned;
+
+          //check if the goal is reached
+          if (this.progress >= this.goal) {
+              // Emit "goalReached" event with the current progress
+              this.emit('goalReached', this.progress);
+          }
+      }
+  }
+
+  const Solution = () => {
+      // define  listener that sends a congratulatory message to the user upon reaching their fitness goal
+      const tracker = new FitnessTracker();
+
+      // congratulatory message
+
+      tracker.on('goalReached', () => {
+              console.log("Congratulations! You have reached your fitness goal.");
+          })
+          // simulate adding exercise
+      tracker.addExercise({ name: "Running", caloriesBurned: 500 });
+      tracker.addExercise({ name: "Weightlifting", caloriesBurned: 600 });
+  };
+
+  Solution();
+
+  export { FitnessTracker, Solution };
