472421d78273aea4fcce6863950d847685dc0e5f
BhuvneshKumarAnand <bharatanand874@gmail.com>
2023-12-30 15:01:26 +0530

    modify problem on events

--- Exploring More Modules/02 Events in NodeJS/Problem/index.js
+++ Exploring More Modules/02 Events in NodeJS/Problem/index.js
@@ -38,3 +38,29 @@ const server = http.createServer((req, res) => {
 });
 
 export default server;
+
+/*
+// Please do not change the prewritten code
+
+import http from "http";
+import fs from "fs";
+
+const server = http.createServer((req, res) => {
+    //  Write your code here
+    if (req.method == "POST") {
+        let body = "";
+        req.on("data", (chunk) => {
+            body += chunk.toString();
+        });
+
+        req.on("end", () => {
+            fs.appendFileSync("data.txt", body)
+            const read = fs.readFileSync("data.txt", "utf-8")
+            console.log(read);
+        })
+    }
+    res.end("data received");
+});
+
+export default server;
+*/
