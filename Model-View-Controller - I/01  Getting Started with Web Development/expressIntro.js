3795ef30d1f6e0a90e31c96154977d169933721b
BhuvneshKumarAnand <bharatanand874@gmail.com>
2024-01-01 01:06:46 +0530

    MVC in ExpressJS

---
+++ Model-View-Controller - I/01  Getting Started with Web Development/expressIntro.js
@@ -0,0 +1,39 @@
+// Express - A web framework
+
+// 1. modern
+// 2. High performance
+// 3. Scalable
+
+
+// Some of most popular web development pattern
+
+// 1. MVC - Separation of concern
+// Model View Controller
+
+// 2. REST API's - Representational Stage Transfer Protocol
+
+// It is used for Cross Platform Support
+
+// Desktop Client <---> REST Server <---> Mobile Client
+
+// 3. Microservices - Highly Scalable Pattern/Architecture
+
+/*
+MVC - Model View Controller
+
+1. Model:
+
+->Represent data
+->Represent Business Logic
+
+2. Views
+
+->User Interface
+->Displays data
+
+3. Controller
+
+->Handles Requests
+->Updates Model
+->Send Responses
+*/
