a1769e2fba5f23b0e8afc34bac6870da292cd39f
BhuvneshKumarAnand <bharatanand874@gmail.com>
2024-01-10 22:02:51 +0530

    Modified problem 5

--- Model-View-Controller - II/Problem 5/user.controller.js
+++ Model-View-Controller - II/Problem 5/user.controller.js
@@ -9,23 +9,6 @@ export const renderUpdateForm = (req, res) => {
 
 // Write your code here
 export const updateUser = (req, res) => {
-    const { name, email, image } = req.body;
-
-    // Validate if the required fields are present
-    if (!name || !email || !image) {
-        return res.status(400).send('Name, email, and image are required');
-    }
-
-    // Assuming you have a single user in the users array
-    const updatedUser = {
-        name: name,
-        email: email,
-        image: image
-    };
-
-    // Update the user data in the model
-    updateUsers(updatedUser);
-
-    // Render the "update-form" view with the updated user data
-    res.render('update-form', { user: updatedUser });
+    updateUsers(req.body);
+    res.render('update-form', { user: req.body });
 };
