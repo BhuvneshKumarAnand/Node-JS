4409d57be35405560890d6b1a1ea4f4cecbbc326
BhuvneshKumarAnand <bharatanand874@gmail.com>
2024-02-01 01:23:26 +0530

    Understanding REST

---
+++ E-comm Project Using REST API/02 Understanding REST/restApi.js
@@ -0,0 +1,26 @@
+// RESTful API
+
+// Representational State Transfer - JSON/XML
+// Stateless Architecture
+// Architectural Guideline
+// Popularly used across different types of systems
+
+// Benefits
+
+// Simplicity
+// Scalable
+// Interoperability
+// support caching
+
+// REST Methods
+
+// GET- get a resource
+// POST- Create a resource
+// PUT- Update a resource
+// DELETE- delete a resource
+
+// Follows CRUD Operation
+
+// One of the primary attributes of RESTful APIs is their capability to enable seamless integration with diverse platforms and systems. RESTful APIs enjoy extensive support across various types of platforms, facilitating smoother communication between servers and clients.
+
+// REST APIs contribute to the scalability of applications by being loosely coupled and independent of clients. This means that the backend API can be scaled independently without affecting the clients. It becomes easier to replicate servers or add more resources to handle increased traffic since the server does not store client-specific information.
