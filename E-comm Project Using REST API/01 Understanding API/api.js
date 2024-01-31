631f97d34f36f38904fa855f76b11ee6427a6209
BhuvneshKumarAnand <bharatanand874@gmail.com>
2024-02-01 01:13:32 +0530

    Understanding API

---
+++ E-comm Project Using REST API/01 Understanding API/api.js
@@ -0,0 +1,15 @@
+// API- Application Programming Interface
+
+// Why not use MVC?
+// due to tight coupling
+// complexity of the application grows
+// difficult to make changes
+// difficult to scale
+
+// API provides Loose coupling
+
+// Types of API
+
+// 1. Simple Object Access Protocol {SOAP} API's - Oldest API
+// 2. REST:API (Lightweight, fast, Easy to work with)
+// 3. GraphQL (Latest at least for now)
