dcd48248e7469fb61b5a21cc6ef99e104fb6ac38
BhuvneshKumarAnand <bharatanand874@gmail.com>
2024-02-21 02:33:44 +0530

    problems on Mongo db

---
+++ MongoDB with NodeJS - I/Problem 3/src/config/mongodb.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+
+// for ipv4 configuration, use this url: "mongodb://127.0.0.1:27017"
+const url = "mongodb://localhost:27017";
+
+let client;
+export const connectToMongoDB = () => {
+  MongoClient.connect(url)
+    .then((clientInstance) => {
+      client = clientInstance;
+      console.log("Mongodb is connected");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const getDB = () => {
+  return client.db("ExpenZap");
+};
